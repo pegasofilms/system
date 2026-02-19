af9958f9d270e4e459170a128d333333171dd01b
Ricky Didier Peralta reyes <didier_peralta@hotmail.com>
2026-02-19 13:45:59 -0600

    Mover Navbar de App.vue a Home.vue para que no aparezca en todas las páginas

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <Navbar />
     <main>
-  <RouterView />
+      <RouterView />
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import Navbar from '@/components/Navbar.vue';
 </script>
 
 <style scoped>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="pegaso-films-page">
+    <Navbar />
     <HeroSection />
     <ServiciosSection />
     <ComoTrabajamosSection />
@@ -11,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
 import HeroSection from '@/components/sections/HeroSection.vue';
 import ServiciosSection from '@/components/sections/ServiciosSection.vue';
 import ComoTrabajamosSection from '@/components/sections/ComoTrabajamosSection.vue';
